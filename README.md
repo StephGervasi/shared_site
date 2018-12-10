28a363f47e940770db1d364c0945b28cc0ac4233
StephGervasi <steph.gervasi@gmail.com>
2018-12-10 18:41:32 -0500

    update starting code and deleted readme

--- README.md
+++
@@ -1,3 +0,0 @@
-# shared_site
-source files for shared R club website
-test1 from R to github
--- startingcode.R
+++ startingcode.R
@@ -1,3 +1,6 @@
 install.packages("blogdown")
 library(blogdown)
 blogdown::install_hugo()
+blogdown::new_site() 
+# Note that you need to get rid of the gitignore and readme file because the new_site function requires that there be an empty repository for the project
+
